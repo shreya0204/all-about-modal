2d4f5081eb096ba75c495867f4f6735e1d9490db
Shreya Agarwal <www.shreyaagarwal.com@gmail.com>
2025-06-22 18:28:38 +0530

    style close button

--- templates/block-templates/modal.php
+++ templates/block-templates/modal.php
@@ -26,7 +26,21 @@
 	<?php echo wp_kses_post( $inner_blocks ); ?>
 	<tp-modal overlay-click-close="yes" modal-id="<?php echo esc_attr( $attributes['modalId'] ); ?>" class="easy-wp-modal__tp-modal">
 		<tp-modal-close>
-			<button>x</button>
+			<button>
+				<svg
+					width="20"
+					height="20"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="black"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				>
+					<line x1="18" y1="6" x2="6" y2="18" />
+					<line x1="6" y1="6" x2="18" y2="18" />
+				</svg>
+			</button>
 		</tp-modal-close>
 		<tp-modal-content>
 			<?php
